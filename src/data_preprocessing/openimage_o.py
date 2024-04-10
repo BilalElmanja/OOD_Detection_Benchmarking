fa6f78a2ebb2e7d9fb594a690a9c0256dbedbc1f
Bilal El-manja <bilal.el-manja@deel12.irtse-pf.ext>
2024-04-10 15:50:01 +0200

    cifar-100 benchmark done

--- src/data_preprocessing/openimage_o.py
+++ src/data_preprocessing/openimage_o.py
@@ -6,20 +6,20 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
-class CIFAR100(Dataset):
-    def __init__(self, train=True, transform=None):
+class OpenImage_O(Dataset):
+    def __init__(self, train=False, transform=None):
         """
         Args:
             root_dir (string): Directory with all the images (should contain 'train' and 'test' subdirectories).
             train (bool, optional): If True, creates dataset from 'train' folder, otherwise from 'test' folder.
             transform (callable, optional): Optional transform to be applied on a sample.
         """
-        self.root_dir = "../../data/CIFAR-100"
+        self.root_dir = "../../data/OpenImage-O"
         self.train = train
         self.transform = transform
-        self.data_dir = os.path.join(self.root_dir, 'train' if self.train else 'test')
-        self.classes = [d for d in os.listdir(self.data_dir) if os.path.isdir(os.path.join(self.data_dir, d)) and d != '.DS_Store']
-        print(self.classes)
+        self.data_dir = os.path.join(self.root_dir, 'train' if self.train else '')
+        self.classes = [d for d in os.listdir(self.data_dir) if os.path.isdir(os.path.join(self.data_dir, d)) and d != '.DS_Store' and d != 'imglist.txt']
+        # print(self.classes)
         self.class_to_idx = {cls_name: i for i, cls_name in enumerate(self.classes)}
         
         self.image_paths = []
@@ -28,7 +28,7 @@ class CIFAR100(Dataset):
         for cls_name in self.classes:
             cls_dir = os.path.join(self.data_dir, cls_name)
             for img_name in os.listdir(cls_dir):
-                if img_name.split(".")[1] not in ["jpg", "png", "jpeg", "webp"] or img_name == ".DS_Store":
+                if img_name.split(".")[1] not in ["jpg", "png", "jpeg", "webp", "JPEG"] or img_name == ".DS_Store":
                     continue
                 self.image_paths.append(os.path.join(cls_dir, img_name))
                 self.image_labels.append(self.class_to_idx[cls_name])
@@ -48,32 +48,33 @@ class CIFAR100(Dataset):
     
 
 transform = transforms.Compose([
-    # ResNet models expect 3-channel images, but CIFAR-10 is already in this format
+    # ResNet models expect 3-channel images, but OpenImage_O is already in this format
     transforms.Resize((32, 32)),  # Ensuring the image size is 32x32
     transforms.RandomHorizontalFlip(),  # A common augmentation for image data
     transforms.ToTensor(),  # Convert images to PyTorch tensors
-    # Normalize each channel of the CIFAR-10 images using mean and std
+    # Normalize each channel of the OpenImage_O images using mean and std
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
  
 ])
 
-def get_train_dataset_cifar100():
-    # Initialize the CIFAR10 datasets for training and testing
-    train_dataset = CIFAR100( train=True, transform=transform)
-    print("the length of the CIFARO-100 Training dataset : ", len(train_dataset))
+def get_train_dataset_OpenImage_O():
+    # Initialize the SSB_hard datasets for training and testing
+    train_dataset = OpenImage_O( train=True, transform=transform)
+    print("the length of the OpenImage_O Training dataset : ", len(train_dataset))
     # Create the DataLoaders for training and testing
     train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=4)
     return train_loader
 
-def get_test_dataset_cifar100():
-    # Initialize the CIFAR10 datasets for training and testing
-    test_dataset = CIFAR100( train=False, transform=transform)
-    print("the length of the CIFAR-100 Test dataset : ", len(test_dataset))
+def get_test_dataset_OpenImage_O():
+    # Initialize the inaturalist datasets for training and testing
+    test_dataset = OpenImage_O( train=False, transform=transform)
+    print("the length of the OpenImage_O Test dataset : ", len(test_dataset))
     # Create the DataLoaders for training and testing
     test_loader = DataLoader(test_dataset, batch_size=128, shuffle=True, num_workers=4)
     return test_loader
 
-
+# train_loader = get_train_dataset_OpenImage_O()
+test_loader = get_test_dataset_OpenImage_O()
 
 
 
